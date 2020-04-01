85450fee05ec9714252b2d89be2ea5fb64d93b7e
wenyang zheng <zwenyang@outlook.com>
2020-03-31 21:59:18 -0400

    1.0

---
+++ cmake-build-debug/CMakeFiles/GitTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitTest.dir/main.cpp.o"
+  "GitTest"
+  "GitTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GitTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GitTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GitTest.
+# This may be replaced when dependencies are built.
