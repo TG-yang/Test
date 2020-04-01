485c70873559a0452a4f538d477bdd8b0b2d67ef
wenyang zheng <zwenyang@outlook.com>
2020-04-01 15:54:32 -0400

    1.2.1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main() {
     string str = "asd";
     cout << str << endl;
 
+    string second = "asd";
+    cout << second << endl;
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
