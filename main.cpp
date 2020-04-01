2d0959a872fbd0dd40a5a9ada79e8a1b53f297de
wenyang zheng <zwenyang@outlook.com>
2020-04-01 15:37:12 -0400

    1.1 Add an output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
 
 int main() {
+    string str = "asd";
+    cout << str << endl;
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
